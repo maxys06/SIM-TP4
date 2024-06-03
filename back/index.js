e591a64af8f49661e2ef7dc89820d49e784e991a
maxys06 <maxysanchez.06@gmail.com>
2024-06-03 20:44:51 -0300

    commit inicial

---
+++ back/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import qs from 'qs';
+
+const port = 5174;
+const app = express();
+
+
+// Definicion de Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+app.set('query parser',
+  (str) => qs.parse(str))
+
+app.get('/', (req, res) => {
+  res.send("Hello World");
+})
+
+// Endpoints
+app.post('/montecarloBicicleteria', async (req, res) => {
+  try{
+    
+    res.json(null);
+    
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Error de Servidor' });
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Inventario de Bicicletas escuchando en ${port}`)
+})
